197e465538ef377a02ce4636cc3ed68d43d8282c
yoojua <yojuaa@gmail.com>
2025-12-09 18:30:43 +0900

    temp: 커밋 꼬인거 정리

--- src/main/java/com/Board/Answer.java
+++ src/main/java/com/Board/Answer.java
@@ -1,4 +1,18 @@
 package com.Board;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
 public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
 }
